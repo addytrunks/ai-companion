87ad0fdff5ed77a6518629baeb61b4b8c9a29acc
Adhithya Srivatsan <addytrunks@gmail.com>
2023-07-31 13:22:21 +0530

    feat: sidebar(both mobile & desktop)

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/navbar"
+import Sidebar from "@/components/sidebar"
 
 export default function RootLayout({
     children,
@@ -8,6 +9,9 @@ export default function RootLayout({
     return (
       <div className="h-full">
         <Navbar/>
+        <div className="hidden md:flex mt-16 w-20 flex-col inset-y-0 fixed">
+          <Sidebar/>
+        </div>
         <main className="md:pl-20 pt-16 h-full">
             {children}
         </main>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,6 +8,7 @@ import {UserButton} from '@clerk/nextjs'
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { ThemeToggle } from "@/components/ui/theme-toggle"
+import MobileSidebar from "./mobile-sidebar"
 
 const font = Poppins({weight:"600",subsets:["latin"]})
 
@@ -16,7 +17,7 @@ const Navbar = () => {
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
 
         <div className="flex items-center">
-            <Menu className="block md:hidden"/>
+            <MobileSidebar/>
             <Link href='/'>
                 <h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary",font.className)}>companion.ai</h1>
             </Link>
