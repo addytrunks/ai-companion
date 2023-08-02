faa6728d38e7dbbcb144786f53cf5becb1dda6fa
Adhithya Srivatsan <addytrunks@gmail.com>
2023-08-02 17:37:29 +0530

    feat: companions list

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,15 +1,43 @@
 import Categories from "@/components/categories";
+import Companions from "@/components/companions";
 import SearchInput from "@/components/search-input";
 import prismadb from "@/lib/prismadb";
 
-export default async function Home() {
+interface Props{
+  searchParams:{
+    categoryId:string,
+    name:string
+  }
+}
+
+export default async function Home({searchParams}:Props) {
 
   const categories = await prismadb.category.findMany({})
+
+  const companions = await prismadb.companion.findMany({
+    where:{
+      categoryId:searchParams.categoryId,
+      name:{
+        search:searchParams.name
+      },
+    },
+    orderBy:{
+      createdAt:'desc'
+    },
+    include:{
+      _count:{
+        select:{
+          messages:true
+        }
+      }
+    }
+  })
 
   return (
     <div className="h-full p-4 space-y-2">
         <SearchInput/>
         <Categories data={categories}/>
+        <Companions data={companions}/>
     </div>
   )
 }
