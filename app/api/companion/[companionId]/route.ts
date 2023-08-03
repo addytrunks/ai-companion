b3efd2bdcaeeee9c061362fd7083c8e5b17e8d93
Adhithya Srivatsan <addytrunks@gmail.com>
2023-08-03 11:40:24 +0530

    add: delete companion API route

--- app/api/companion/[companionId]/route.ts
+++ app/api/companion/[companionId]/route.ts
@@ -1,5 +1,5 @@
 import prismadb from "@/lib/prismadb"
-import { currentUser } from "@clerk/nextjs"
+import { auth, currentUser } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
 
 export const PATCH = async(req:Request,{params}:{params:{companionId:string}}) => {
@@ -41,4 +41,32 @@ export const PATCH = async(req:Request,{params}:{params:{companionId:string}}) =
         console.log('COMPANION_PATCH',error)
         return new NextResponse('Internal Error',{status:500})
     }
+}
+
+export const DELETE = async(req:Request,{params}:{params:{companionId:string}}) => {
+
+    try {
+        const {userId} = auth()
+
+        if(!params.companionId){
+            return new NextResponse('Companion ID is required',{status:400})
+        }
+
+        if(!userId){
+            return new NextResponse('Unauthorized',{status:401})
+        }
+
+        const companion = await prismadb.companion.delete({
+            where:{
+                userId,
+                id:params.companionId
+            }
+        })
+
+        return NextResponse.json(companion)
+    } catch (error) {
+        console.log('COMPANION_DELETE',error)
+        return new NextResponse('Internal Error',{status:500})
+    }
+
 }
